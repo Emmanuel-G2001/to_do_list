74fe52f479df57f8e5a4babf7d62061a405ef7f8
Emmanuel Garzon <emmanuelgarzon2001@hotmail.com>
2024-11-24 14:47:15 -0500

    Initial commit in dev and update in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:5000/tasks')
+            .then(response => setTasks(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    const addTask = () => {
+        axios.post('http://127.0.0.1:5000/tasks', { task: newTask })
+            .then(() => {
+                setNewTask('');
+                axios.get('http://127.0.0.1:5000/tasks').then(res => setTasks(res.data));
+            })
+            .catch(error => console.error(error));
+    };
+
+    const deleteTask = (id) => {
+        axios.delete(`http://127.0.0.1:5000/tasks/${id}`)
+            .then(() => axios.get('http://127.0.0.1:5000/tasks').then(res => setTasks(res.data)))
+            .catch(error => console.error(error));
+    };
+
+    return (
+        <div>
+            <h1>To-Do List</h1>
+            <input
+                type="text"
+                value={newTask}
+                onChange={(e) => setNewTask(e.target.value)}
+                placeholder="New Task"
+            />
+            <button onClick={addTask}>Add</button>
+            <ul>
+                {tasks.map(task => (
+                    <li key={task.id}>
+                        {task.task} <button onClick={() => deleteTask(task.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
